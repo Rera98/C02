e97701e24fb78be05345a11fc4f51e0fe3c46ec9
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-21 11:21:48 +0100

    commit

---
+++ even_or_odd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    if (n % 2 == 0) {
+        printf("%d est pair.\n", n);
+    } else {
+        printf("%d est impair.\n", n);
+    }
+
+    return 0;
+}
